8c3695bc72daab982fac5a71b9ea765673b3e2bd
Eduardo Brito <alu0100783315@ull.edu.es>
2021-03-01 12:54:18 +0000

    Encryption working

--- src/index.ts
+++ src/index.ts
@@ -9,4 +9,18 @@ class Vigenere {
         return text.toUpperCase().replace(/[^A-Z]/g, "")
     }
 
+    encrypt(plainText: string): string {
+        return Array.prototype.map.call(Vigenere.formatText(plainText), (letter: string, index: number): string => {
+            return String.fromCharCode((letter.charCodeAt(0) + this.key.charCodeAt(index % this.key.length) - 130) % 26 + 65)
+        }).join("");
+    }
 }
+
+let original: string = "DES AST RES";
+
+console.log(`Mensaje original: ${original}`);
+
+let vig: Vigenere = new Vigenere("SOL");
+let encoded: string = vig.encrypt(original);
+
+console.log(`Mensaje cifrado: ${encoded}`);
